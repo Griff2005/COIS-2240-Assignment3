4b3531ce5722cab797989183b50aaa73bc2eec8a
Griffin Robinson <griffrob1205@gmail.com>
2025-04-05 13:08:01 -0400

    Add Javadoc comments and add tests for retuning

--- test/VehicleRentalTest.java
+++ test/VehicleRentalTest.java
@@ -1,6 +1,5 @@
 package test;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import src.Car;
@@ -10,15 +9,24 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The type Vehicle rental test.
+ */
 public class VehicleRentalTest {
 
     private RentalSystem rentalSystem;
 
+    /**
+     * Sets up.
+     */
     @BeforeEach
     void setUp() {
         this.rentalSystem = RentalSystem.getInstance();
     }
-    
+
+    /**
+     * Test license plate validation.
+     */
     @Test
     void testLicensePlateValidation() {
         Vehicle testVehicle = new Car("Toyota", "Camry", 2020, 5);
@@ -38,16 +46,25 @@ public class VehicleRentalTest {
         assertThrows(IllegalArgumentException.class, () -> testVehicle.setLicensePlate("ZZZ99"));
     }
     
+    /**
+     * Test rent and return vehicle.
+     */
     @Test
     void testRentAndReturnVehicle() {
+        // Rent
         Vehicle testVehicle = new Car("Toyota", "Camry", 2020, 5);
         Customer testCustomer = new Customer(001, "George");
         
         assertEquals(Vehicle.VehicleStatus.AVAILABLE, testVehicle.getStatus());
-        
         assertTrue(rentalSystem.rentVehicle(testVehicle, testCustomer, LocalDate.now(), 500));
         assertEquals(Vehicle.VehicleStatus.RENTED, testVehicle.getStatus());
         
         assertFalse(rentalSystem.rentVehicle(testVehicle, testCustomer, LocalDate.now(), 500));
+        
+        // Return
+        assertTrue(rentalSystem.returnVehicle(testVehicle, testCustomer, LocalDate.now(), 20));
+        assertEquals(Vehicle.VehicleStatus.AVAILABLE, testVehicle.getStatus());
+        
+        assertFalse(rentalSystem.returnVehicle(testVehicle, testCustomer, LocalDate.now(), 20));
     }
 }
