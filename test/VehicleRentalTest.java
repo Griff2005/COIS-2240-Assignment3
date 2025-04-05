a3b756e7d4c7da79a19f13dec8c45e62cb482cad
Griffin Robinson <griffrob1205@gmail.com>
2025-04-05 13:29:19 -0400

    Add tests for singleton pattern in RentalSystem and refactor setup in VehicleRentalTest

--- test/VehicleRentalTest.java
+++ test/VehicleRentalTest.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import src.Car;
 import src.*;
-
 import java.time.LocalDate;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,16 +14,6 @@ import static org.junit.jupiter.api.Assertions.*;
  * The type Vehicle rental test.
  */
 public class VehicleRentalTest {
-
-    private RentalSystem rentalSystem;
-
-    /**
-     * Sets up.
-     */
-    @BeforeEach
-    void setUp() {
-        this.rentalSystem = RentalSystem.getInstance();
-    }
 
     /**
      * Test license plate validation.
@@ -52,6 +43,7 @@ public class VehicleRentalTest {
     @Test
     void testRentAndReturnVehicle() {
         // Rent
+        RentalSystem rentalSystem = RentalSystem.getInstance();
         Vehicle testVehicle = new Car("Toyota", "Camry", 2020, 5);
         Customer testCustomer = new Customer(001, "George");
         
@@ -66,5 +58,20 @@ public class VehicleRentalTest {
         assertEquals(Vehicle.VehicleStatus.AVAILABLE, testVehicle.getStatus());
         
         assertFalse(rentalSystem.returnVehicle(testVehicle, testCustomer, LocalDate.now(), 20));
+    }
+
+    /**
+     * Test singleton rental system.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void testSingletonRentalSystem() throws Exception{
+        Constructor<RentalSystem> constructor = RentalSystem.class.getDeclaredConstructor();
+        
+        assertEquals(Modifier.PRIVATE, constructor.getModifiers());
+        
+        assertNotNull(RentalSystem.getInstance());
+        
     }
 }
